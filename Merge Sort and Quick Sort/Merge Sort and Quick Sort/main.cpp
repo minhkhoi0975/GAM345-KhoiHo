c511430cf2341b73392f62afa5e482260cf2dd2a
Khoi Ho <minhkhoi0975@gmail.com>
2022-03-29 01:00:04 -0400

    Test the average completion time of sorting algorithms.

--- Merge Sort and Quick Sort/Merge Sort and Quick Sort/main.cpp
+++ Merge Sort and Quick Sort/Merge Sort and Quick Sort/main.cpp
@@ -1,6 +1,7 @@
 // Name(s): Khoi Ho, Matt Mills, Ethem Ficici
 
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include "Vector.h"
 #include "Random.h"
@@ -203,7 +204,10 @@ int Partition(Vector<T>& vector, const int& start, const int& end)
 	return leftIndex;
 }
 
-// Sorting time measurement.
+// -------------------------------
+// Sorting time measurement
+// -------------------------------
+
 typedef void (*SortingFunction)(Vector<int>& vector);
 
 template<class T>
@@ -217,15 +221,111 @@ long long MeasureSortingTime(const Vector<T>& vector, SortingFunction sortingFun
 	sortingFunction(copy);
 	auto endTime = std::chrono::high_resolution_clock::now();
 
-	// Return the sorting time.
+	// Return the sorting time in nanoseconds.
 	return std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
 }
 
+bool GenerateDataAverageCase(const std::string& dataFileName, const int& minN, const int& maxN, Random& random)
+{
+	std::fstream dataFile(dataFileName, std::ios::out | std::ios::trunc);
+
+	if (!dataFile)
+		return false;
+
+	dataFile << "#n\tQuickSort(ns)\tMergeSortList(ns)\tSelectionSort(ns)" << std::endl;
+
+	for (int n = minN; n <= maxN; n++)
+	{
+		// Resize the vector.
+		Vector<int> numbers;
+		numbers.Resize(n);
+		 
+		std::cout << "n = " << n << std::endl;
+		
+		// Measure the average time of each sorting algorithm.
+		long long quickSortAverageTime = 0;
+		long long mergeSortAverageTime = 0;
+		long long selectionSortAverageTime = 0;
+
+		for (int i = 0; i < 100; i++)
+		{
+			// Generate random elements for the vector.
+			for (int i = 0; i < n; i++)
+			{
+				numbers[i] = random.Integer(1, n);
+			}
+
+			// Measure and add the time to the total time.
+			quickSortAverageTime += MeasureSortingTime(numbers, QuickSortRecursive);
+			mergeSortAverageTime += MeasureSortingTime(numbers, MergeSortRecursive);
+			selectionSortAverageTime += MeasureSortingTime(numbers, SelectionSort);
+		}
+
+		// Calculate the average time.
+		quickSortAverageTime /= 100;
+		mergeSortAverageTime /= 100;
+		selectionSortAverageTime /= 100;
+
+		// Write the average times to the file.
+		dataFile << n << '\t' << quickSortAverageTime << '\t' << mergeSortAverageTime << '\t' << selectionSortAverageTime << '\n';
+	}
+
+	dataFile.close();
+
+	return true;
+}
+
+// outPutFile1 is for Quick Sort vs Merge Sort, and outputFile2 is for QuickSort vs Selection Sort.
+bool GenerateScriptAverageCase(const std::string& scriptFileName, const std::string& dataFileName, const std::string& outputFileName1, const std::string& outputFileName2)
+{
+	std::fstream scriptFile(scriptFileName, std::ios::out | std::ios::trunc);
+
+	if (!scriptFile)
+		return false;
+
+	// Print out the script.
+	scriptFile
+		<< "reset" << std::endl
+		<< "set terminal png size 1920,1080" << std::endl
+		<< "set output '" << outputFileName1 << "'" << std::endl
+		<< std::endl
+		<< "set title 'Average Completion Time of Quick Sort vs Merge Sort'" << std::endl
+		<< "set xlabel 'Size (N)'" << std::endl
+		<< "set ylabel 'Average time (ns)'" << std::endl
+		<< "set xzeroaxis" << std::endl
+		<< "set key inside top left" << std::endl
+		<< std::endl
+		<< "plot \"" << dataFileName << "\" using 1:2 title 'Quick Sort' lw 1 with lines, \"" << dataFileName << "\" using 1:3 title 'Merge Sort' lw 1 with lines" << std::endl
+
+		<< std::endl
+
+		<< "reset" << std::endl
+		<< "set terminal png size 1920,1080" << std::endl
+		<< "set output '" << outputFileName2 << "'" << std::endl
+		<< std::endl
+		<< "set title 'Average Completion Time of Quick Sort vs Selection Sort'" << std::endl
+		<< "set xlabel 'Size (N)'" << std::endl
+		<< "set ylabel 'Average time (ns)'" << std::endl
+		<< "set xzeroaxis" << std::endl
+		<< "set key inside top left" << std::endl
+		<< std::endl
+		<< "plot \"" << dataFileName << "\" using 1:2 title 'Quick Sort' lw 1 with lines, \"" << dataFileName << "\" using 1:4 title 'Selection Sort' lw 1 with lines" << std::endl;
+
+	// Close the file.
+	scriptFile.close();
+
+	return true;
+}
+
+
 int main()
 {
+	
 	// Create a vector of random numbers.
 	Random random;
-	const int MAX_VECTOR_SIZE = 100;
+
+	/*
+	const int MAX_VECTOR_SIZE = 1000;
 	const int MIN_INT_VAL = 1;
 	const int MAX_INT_VAL = 100;
 
@@ -253,6 +353,11 @@ int main()
 
 		std::cout << "Size = " << arrSize << "\tQuick Sort: " << quickSortTime << "ns" << "\tMerge Sort: " << mergeSortTime << "ns" << "\tSelection Sort: " << selectionSortTime << "ns" << std::endl;
 	}
+	*/
+
+	GenerateDataAverageCase("average_case_data.txt", 1, 200, random);
+	GenerateScriptAverageCase("average_case_script.txt", "average_case_data.txt", "average_case_output_quicksort_vs_mergesort.png", "average_case_output_quicksort_vs_selectionsort.png");
+	system("gnuplot average_case_script.txt");
 
 	// PrintVector(numbers);
 }
