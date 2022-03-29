a990dfd8c2812ad895ca5a432028f2218fb7ff6b
Khoi Ho <minhkhoi0975@gmail.com>
2022-03-29 00:26:49 -0400

    Write a function that measures sorting time.

--- Merge Sort and Quick Sort/Merge Sort and Quick Sort/main.cpp
+++ Merge Sort and Quick Sort/Merge Sort and Quick Sort/main.cpp
@@ -1,87 +1,9 @@
 // Name(s): Khoi Ho, Matt Mills, Ethem Ficici
 
 #include <iostream>
+#include <chrono>
 #include "Vector.h"
 #include "Random.h"
-
-void Merge(int* numbers, int start, int mid, int end)
-{
-	// Create a temporary array.
-	int tempArrSize = end - start + 1;
-	int* tempArr = new int[tempArrSize];
-
-	// Put the elements into the temporary array.
-	int i = 0;
-	int leftIndex = start;
-	int rightIndex = mid + 1;
-	while (leftIndex <= mid && rightIndex <= end)
-	{
-		if (numbers[leftIndex] <= numbers[rightIndex])
-		{
-			tempArr[i] = numbers[leftIndex];
-			++leftIndex;
-		}
-		else
-		{
-			tempArr[i] = numbers[rightIndex];
-			++rightIndex;
-		}
-
-		++i;
-	}
-
-	// Deal with the cases when there are still some elements left in the left subarray.
-	while (leftIndex <= mid)
-	{
-		tempArr[i] = numbers[leftIndex];
-		++leftIndex;
-		++i;
-	}
-
-	// Deal with the cases when there are still some elements left in the right subarray.
-	while (rightIndex <= end)
-	{
-		tempArr[i] = numbers[rightIndex];
-		++rightIndex;
-		++i;
-	}
-	
-	// Copy the elements of the temporary array back to the original array.
-	for (int i = 0; i < tempArrSize; ++i)
-	{
-		numbers[start + i] = tempArr[i];
-	}
-
-	// Delete the temporary array to avoid memory leak.
-	delete[] tempArr;
-}
-
-void MergeSortRecursive(int* numbers, int start, int end)
-{
-	// Don't sort if the size of the subarray is 1 or lower.
-	if (end - start <= 0)
-		return;
-
-	int mid = start + (end - start) / 2;
-
-	// Sort the left half of the subarray.
-	MergeSortRecursive(numbers, start, mid);
-
-	// Sort the right half of the subarray.
-	MergeSortRecursive(numbers, mid + 1, end);
-
-	// Merge the two halves together.
-	Merge(numbers, start, mid, end);
-}
-
-void PrintArray(int* arr, int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << arr[i] << ' ';
-	}
-	std::cout << std::endl;
-}
 
 template <class T>
 void Swap(T& a, T& b)
@@ -101,6 +23,29 @@ void PrintVector(Vector<T>& vector)
 		std::cout << vector[i] << ' ';
 	}
 	std::cout << std::endl;
+}
+
+// -------------------------------
+// Selection Sort
+// -------------------------------
+
+template <class T>
+void SelectionSort(Vector<T>& vector)
+{
+	for (int i = 0; i < vector.Size() - 1; i++)
+	{
+		int minIndex = i;
+
+		for (int j = i + 1; j < vector.Size(); j++)
+		{
+			if (vector[j] < vector[minIndex])
+			{
+				minIndex = j;
+			}
+		}
+
+		Swap(vector[i], vector[minIndex]);
+	}
 }
 
 // -------------------------------
@@ -258,28 +203,56 @@ int Partition(Vector<T>& vector, const int& start, const int& end)
 	return leftIndex;
 }
 
+// Sorting time measurement.
+typedef void (*SortingFunction)(Vector<int>& vector);
+
+template<class T>
+long long MeasureSortingTime(const Vector<T>& vector, SortingFunction sortingFunction)
+{
+	// Create a copy of the vector.
+	Vector<T> copy(vector);
+
+	// Measure the sorting time.
+	auto startTime = std::chrono::high_resolution_clock::now();
+	sortingFunction(copy);
+	auto endTime = std::chrono::high_resolution_clock::now();
+
+	// Return the sorting time.
+	return std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+}
+
 int main()
 {
 	// Create a vector of random numbers.
 	Random random;
-	const int VECTOR_SIZE = 20;
+	const int MAX_VECTOR_SIZE = 100;
 	const int MIN_INT_VAL = 1;
 	const int MAX_INT_VAL = 100;
 
-	Vector<int> numbers;
-	numbers.Resize(VECTOR_SIZE);
-	for (int i = 0; i < VECTOR_SIZE; i++)
+	for (int arrSize = 2; arrSize <= MAX_VECTOR_SIZE; ++arrSize)
 	{
-		numbers[i] = random.Integer(MIN_INT_VAL, MAX_INT_VAL);
+		Vector<int> numbers;
+		numbers.Resize(arrSize);
+		for (int i = 0; i < arrSize; i++)
+		{
+			numbers[i] = random.Integer(1, arrSize);
+		}
+
+		// PrintVector(numbers);
+
+		// Merge sort.
+		//MergeSortRecursive(numbers);
+
+		// Quick sort.
+		// QuickSortRecursive(numbers);
+
+		// Test sorting time measurement.
+		auto quickSortTime = MeasureSortingTime(numbers, QuickSortRecursive);
+		auto mergeSortTime = MeasureSortingTime(numbers, MergeSortRecursive);
+		auto selectionSortTime = MeasureSortingTime(numbers, SelectionSort);
+
+		std::cout << "Size = " << arrSize << "\tQuick Sort: " << quickSortTime << "ns" << "\tMerge Sort: " << mergeSortTime << "ns" << "\tSelection Sort: " << selectionSortTime << "ns" << std::endl;
 	}
 
-	PrintVector(numbers);
-
-	// Merge sort.
-	//MergeSortRecursive(numbers);
-
-	// Quick sort.
-	QuickSortRecursive(numbers);
-
-	PrintVector(numbers);
+	// PrintVector(numbers);
 }
